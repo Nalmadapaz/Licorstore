76f266bb4c42a9183c13bcc27b9b61c41fc53b12
nahuelalmadapaz <nahuel.almada@hotmail.com>
2023-12-26 19:34:31 -0300

    Bootstrap

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,68 @@
-import React from 'react'
+import React from 'react';
 import Cartwidget from '../Cartwidget/Cartwidget';
 import './Navbar.css';
 
-
 const Navbar = () => {
   return (
-    <header>
-      <h1>Licorstore</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img
+            src="./img/logo.jpg"
+            width="100"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Licorstore Logo"
+          />
+          LiquorStore
+        </a>
 
-      <nav>
-        <ul>
-          <li>Vinos</li>
-          <li>Whiskies</li>
-          <li>Gin</li>
-          <li>Cervezas</li>
-          <li>Aperitivos</li>
-        </ul>
-      </nav>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <Cartwidget/>
-    </header>
-  )
-}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Vinos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Whiskies
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Gin
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Cervezas
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Aperitivos
+              </a>
+            </li>
+          </ul>
+        </div>
 
-export default Navbar
+        <Cartwidget />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
