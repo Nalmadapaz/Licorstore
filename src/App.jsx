9a011f2307fddfabd7e62d226a675acb5ceb61a8
nahuelalmadapaz <nahuel.almada@hotmail.com>
2024-02-13 10:41:16 -0300

    proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CarritoProvider } from './context/CarritoContext';
+import Cart from './components/Cart/Cart';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<ItemListContainer/>} />
-        <Route path='/categoria/:idCategoria' element={<ItemListContainer/>} />
-        <Route path='/item/:idItem' element={<ItemDetailContainer/>} />
-      </Routes>
+        <CarritoProvider>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/categoria/:idCategoria' element={<ItemListContainer />} />
+            <Route path='/item/:idItem' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart/>} />
+          </Routes>
+        </CarritoProvider>
       </BrowserRouter>
     </>
   )
--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,9 +1,9 @@
 const productos = [
-    {id:"1", nombre: "Terrazas De Los Andes", precio: 8000, img:"https://www.boozebud.com/cdn/shop/products/1660537522759Terrazas_Malbec-350-937.jpg?v=1680599493&width=713",idCat: "1"},
-    {id:"2", nombre: "Fernet", precio: 5000, img:"https://www.boozebud.com/cdn/shop/products/1580949646800slkalsa-350-937.jpg?v=1680587510&width=713",idCat: "5"},
-    {id:"3", nombre: "Bombay Sapphire", precio: 6200, img:"https://www.boozebud.com/cdn/shop/products/16697801227111-350-937.jpg?v=1680580343&width=713",idCat: "3"},
-    {id:"4", nombre: "Red Label", precio: 6800, img:"https://www.boozebud.com/cdn/shop/products/1609811075043ffdf3-350-937.jpg?v=1680592549&width=713",idCat: "2"},
-    {id:"5", nombre: "Heineken", precio: 1500, img:"https://www.boozebud.com/cdn/shop/products/Heineken-Stubby-350-937.gif?v=1680601656&width=713",idCat: "4"},
+    {id:"1", nombre: "Terrazas De Los Andes", precio: 8000, img:"https://www.boozebud.com/cdn/shop/products/1660537522759Terrazas_Malbec-350-937.jpg?v=1680599493&width=713",idCat: "1", stock: 100},
+    {id:"2", nombre: "Fernet", precio: 5000, img:"https://www.boozebud.com/cdn/shop/products/1580949646800slkalsa-350-937.jpg?v=1680587510&width=713",idCat: "5", stock: 100},
+    {id:"3", nombre: "Bombay Sapphire", precio: 6200, img:"https://www.boozebud.com/cdn/shop/products/16697801227111-350-937.jpg?v=1680580343&width=713",idCat: "3", stock: 100},
+    {id:"4", nombre: "Red Label", precio: 6800, img:"https://www.boozebud.com/cdn/shop/products/1609811075043ffdf3-350-937.jpg?v=1680592549&width=713",idCat: "2", stock: 100},
+    {id:"5", nombre: "Heineken", precio: 1500, img:"https://www.boozebud.com/cdn/shop/products/Heineken-Stubby-350-937.gif?v=1680601656&width=713",idCat: "4", stock: 100},
 ]
 
 export const getProductos = () => {
